c9ba9f60f28f63752edbf37f8ba140428fab5822
Nonso Wogu <recreatesuccess@gmail.com>
2017-10-10 17:33:34 -0400

    Update util.py
    Updated util.py

--- csi4106_a1/searchdir/util.py
+++ csi4106_a1/searchdir/util.py
@@ -5,8 +5,6 @@
 
 from operator import attrgetter
 
-import Queue
-
 #Queue - Implementation of the data structure Queue
 class Queue:
     # initializes the current data structure
@@ -16,7 +14,7 @@ class Queue:
 
     # returns the elements of the current data structure
     def show(self):
-        return self.items
+        return self.list
 
     # returns a boolean indicating whether the current data structure is empty or not
     def isEmpty(self):
@@ -25,6 +23,7 @@ class Queue:
     # add the element item to the current data structure
     def enqueue(self, item):
         self.items.insert(0,item)
+
 
     # removes an element from the current data structure
     def dequeue(self):
@@ -36,68 +35,73 @@ class Queue:
 
     # returns a boolean value that indicates if the element item is contained in the current data structure
     def __contains__(self, item):
-        return item in self.items
+        return item in self.list
 
 
 #Priority Queue Implementation of the data structure PriorityQueue
 class PriorityQueue:
     # initializes the data structure
     def __init__(self, fct):
-        self.q = Queue.PriorityQueue
+        self.items = []
 
     # returns the elements of the current data structure
     def show(self):
-    # TO COMPLETE
+        return self.list
 
     # returns a boolean indicating whether the current data structure is empty or not
     def isEmpty(self):
-    # TO COMPLETE
+        return self.list == []
 
     # add the element item to the current data structure
     def enqueue(self, item):
-    # TO COMPLETE
+        self.items.append(item)
 
     # removes an element from the current data structure
     def dequeue(self):
-    # TO COMPLETE
+        min = 0
+        for i in range(1, len(self.list)):
+            if self.list[i].f < self.list[min].f:
+                min = i
+        item = self.list.pop(min)
+        return item
 
     # returns the size of the current data structure (the number of elements)
     def size(self):
-    # TO COMPLETE
+        return len(self.list)
 
     # returns a boolean value that indicates if the element item is contained in the current data structure
     def __contains__(self, item):
-    # TO COMPLETE
+        return item in self.list
 
 #Stack - Implementation of the data structure Stack
 class Stack:
     # initializes the data structure
     def __init__(self):
-    # TO COMPLETE
+        self.items = []
 
     # returns the elements of the current data structure
     def show(self):
-    # TO COMPLETE
+        return self.list
 
     # returns a boolean indicating whether the current data structure is empty or not
     def isEmpty(self):
-    # TO COMPLETE
+        return self.items == []
 
     # add the element item to the current data structure
     def push(self, item):
-    # TO COMPLETE
+        self.items.append(item)
 
     # removes an element from the current data structure
     def pop(self):
-    # TO COMPLETE
+        return self.items.pop()
 
     # returns the size of the current data structure (the number of elements)
     def size(self):
-    # TO COMPLETE
+        return len(self.items)
 
     # returns a boolean value that indicates if the element item is contained in the current data structure
     def __contains__(self, item):
-    # TO COMPLETE
+        return item in self.list
 
 
 #Prints results for search alorithms
